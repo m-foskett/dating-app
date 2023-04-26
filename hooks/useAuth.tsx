f68690409fc907bd3d35f54e32139402b4c7b9a8
Mark Foskett <markryanfoskett@gmail.com>
2023-04-26 21:17:56 +1000

    improved navigation/auth types

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,66 +1,71 @@
-import { View, Text } from 'react-native'
 import React, { createContext, useContext, useEffect, useMemo, useState } from 'react'
-// import * as AuthSession from 'expo-auth-session';
 import * as Google from 'expo-auth-session/providers/google';
 import { GoogleAuthProvider, signInWithCredential, onAuthStateChanged, signOut } from 'firebase/auth';
 import * as WebBrowser from 'expo-web-browser';
 import { auth } from "../firebase"
-
-WebBrowser.maybeCompleteAuthSession();
-
-const AuthContext = createContext({
-// Initial State of Context
-})
-// Maybe add expoClientId
+import { AuthContextType } from '../types/types';
+import PropTypes from "prop-types";
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+WebBrowser.maybeCompleteAuthSession(); // Not sure if needed
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Auth Configuration
 const config = {
     androidClientId: "690008863159-6t38dunonnp13ogkeo8uim7a92g6vjuu.apps.googleusercontent.com",
     expoClientId: "690008863159-butstt5apsplg14mc6tuchk1jasjdn0e.apps.googleusercontent.com",
     webClientId: "690008863159-oadl46giabf6k62507dplic3t0ll3dv9.apps.googleusercontent.com",
     responseType: "id_token",
 }
-
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Custom Context for Auth
+// Initial state of null
+const AuthContext = createContext<AuthContextType | null>(null);
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Custom HOC - AuthProvider
+// Uses children prop (the Stack Navigator)
 export const AuthProvider = ({ children }) => {
-    const [userName, setUserName] = useState(null);
-    const [userPhoto, setUserPhoto] = useState(null);
-    const [userUID, setUserUID] = useState(null);
-    const [loadingInitial, setLoadingInitial] = useState(true);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    // async ()
-    // const [request, response, promptAsync] = Google.useIdTokenAuthRequest(
-    //     {
-    //     clientId: config.androidClientId,
-    //     },
-    // );
-    // Maybe add redirect uri with the make uri function
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // State Variables
+    const [userName, setUserName] = useState<string | null>(null);
+    const [userPhoto, setUserPhoto] = useState<string | null>(null);
+    const [userUID, setUserUID] = useState<string>(null);
+    const [loadingInitial, setLoadingInitial] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>(null);
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Login via Google Auth:
+    // Load an authorization request.
+    // Returns: loaded request, response, prompt method
+    // When the prompt method completes, then the response will be fulfilled.
     const [request, response, promptAsync] = Google.useAuthRequest(config);
-
-    async function signInWithGoogle () {
+    async function signInWithGoogle() {
         setLoading(true);
-        await promptAsync();
+        if (request) {
+          await promptAsync();
+        }
     }
-
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Logout Function
     const logout = () => {
         setLoading(true);
         signOut(auth)
-            .catch(error => setError(error))
-            .finally(() => setLoading(false));
+        .catch((error) => setError(error))
+        .finally(() => setLoading(false));
     };
-
-    React.useEffect(() => {
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    useEffect(() => {
         if (response?.type === 'success') {
-        const { id_token } = response.params;
-        // const auth = getAuth();
-        //   If successful Google sign in, create a credential and sign in with Firebase Authentication
-        const credential = GoogleAuthProvider.credential(id_token);
-        signInWithCredential(auth, credential);
-        setLoading(false);
+            const { id_token } = response.params;
+            // const auth = getAuth();
+            // If successful Google sign in, create a credential and sign in with Firebase Authentication
+            const credential = GoogleAuthProvider.credential(id_token);
+            signInWithCredential(auth, credential);
+            setLoading(false);
         }
     }, [response]);
-
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // On detecting a change in the authentication state, set the user's properties then set the loading phase to false to avoid UI delay
     // Implicitly returns an unsubscribe to cleanup event listener
-    useEffect(() =>
+    useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             if (user) {
                 // Logged in
@@ -74,22 +79,20 @@ export const AuthProvider = ({ children }) => {
             }
 
             setLoadingInitial(false);
-        }),
-        []
-    );
-
+        })
+    }, [auth]);
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Caches values to reduce rerendering of unchanged variables
-    // const memoedValue = useMemo(() => ({
-    //     userName,
-    //     userPhoto,
-    //     userUID,
-    //     loading,
-    //     logout,
-    //     error,
-    //     signInWithGoogle,
-    // }), [userName, loading, error]);
-
-
+    const memoedValue = useMemo(() => ({
+        userName,
+        userPhoto,
+        userUID,
+        loading,
+        logout,
+        error,
+        signInWithGoogle,
+    }), [userName, loading, error]);
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     return (
         <AuthContext.Provider
             value={{
@@ -105,8 +108,14 @@ export const AuthProvider = ({ children }) => {
             {!loadingInitial && children}
         </AuthContext.Provider>
     );
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 };
-
+AuthProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Custom Hook - useAuth()
 export default function useAuth() {
     return useContext(AuthContext);
-}
+};
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- navigators/StackNavigator.tsx
+++ navigators/StackNavigator.tsx
@@ -1,34 +1,45 @@
-import { View, Text } from 'react-native'
 import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NativeStackNavigationOptions, createNativeStackNavigator } from '@react-navigation/native-stack'
+import useAuth from '../hooks/useAuth';
 import HomeScreen from '../screens/HomeScreen';
 import ChatScreen from '../screens/ChatScreen';
 import LoginScreen from '../screens/LoginScreen';
-import useAuth from '../hooks/useAuth';
 import ModalScreen from '../screens/ModalScreen';
 import MatchScreen from '../screens/MatchScreen';
 import MessageScreen from '../screens/MessageScreen';
-
+import colours from '../config/colours';
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Initialise the Root Stack Navigator
 const Stack = createNativeStackNavigator();
-
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Global Screen Options
+const globalScreenOptions: NativeStackNavigationOptions = {
+  headerShown: false,
+  // headerStyle: { backgroundColor: colours.primary[500]},
+  // headerTitleStyle: { color: colours.primary[950]},
+  // headerTintColor: colours.primary[950],
+  // headerTitleAlign: 'center',
+};
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//   Create Stack Navigator Wrapper
 const StackNavigator = () => {
     const { userName } = useAuth();
-  return (
-    <Stack.Navigator screenOptions={{headerShown: false,}}>
-        {userName ? (
-            <>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Chat" component={ChatScreen} />
-            <Stack.Screen name="Message" component={MessageScreen} />
-            <Stack.Screen name="Modal" component={ModalScreen} options={{presentation: 'modal' }}/>
-            <Stack.Screen name="Match" component={MatchScreen} options={{presentation: 'transparentModal' }}/>
+    return (
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+          {userName ? (
+              <>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Chat" component={ChatScreen} />
+              <Stack.Screen name="Message" component={MessageScreen} />
+              <Stack.Screen name="Modal" component={ModalScreen} options={{presentation: 'modal' }}/>
+              <Stack.Screen name="Match" component={MatchScreen} options={{presentation: 'transparentModal' }}/>
 
-            </>
-        ): (
-            <Stack.Screen name="Login" component={LoginScreen}/>
-        )}
-    </Stack.Navigator>
-  );
+              </>
+          ): (
+              <Stack.Screen name="Login" component={LoginScreen}/>
+          )}
+      </Stack.Navigator>
+    );
 };
-
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 export default StackNavigator
