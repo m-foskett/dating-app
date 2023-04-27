71289f0cfebb65b70faac90ffa244cec2a56cc44
Mark Foskett <markryanfoskett@gmail.com>
2023-04-27 22:11:18 +1000

    update home and login screen styling

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,32 +1,27 @@
-import { View, Text, Button, ImageBackground, TouchableOpacity } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import { View, Text, ImageBackground, TouchableOpacity } from 'react-native'
+import React from 'react'
 import useAuth from '../hooks/useAuth'
-import { useNavigation } from '@react-navigation/native';
 
 const LoginScreen = () => {
-  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // Navigation Prop
-  const navigation = useNavigation();
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Custom Auth Hook: useAuth()
   const { signInWithGoogle, } = useAuth();
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   return (
     <View className='flex-1'>
+      {/* Image Background */}
       <ImageBackground
         resizeMode='cover'
         className="flex-1"
-        source={{ uri: "https://tinder.com/static/tinder.png"}}
+        source={require('../assets/login_bg.png')}
       >
-        <TouchableOpacity className="absolute left-20 bottom-40 w-52 bg-white p-4 rounded-2xl" onPress={signInWithGoogle}>
-          <Text className='font-semibold text-center'>Sign in and start dating!</Text>
+        <TouchableOpacity className="absolute left-20 bottom-40 w-52 bg-primary-950 p-4 rounded-2xl" onPress={signInWithGoogle}>
+          <Text className='text-white text-md font-semibold text-center'>Sign in to start dating!</Text>
         </TouchableOpacity>
-
       </ImageBackground>
-      {/* <Text>{loading ? "Loading..." : "Login to App"}</Text>
-      <Button title='Login' onPress={() => {signInWithGoogle();}}></Button> */}
     </View>
   )
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 }
 
 export default LoginScreen
