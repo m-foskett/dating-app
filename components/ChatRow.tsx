c6f2b159af6f7379914d1154180dec0705d71331
Mark Foskett <markryanfoskett@gmail.com>
2023-04-28 02:49:23 +1000

    fixed custom chat screen components styling and type checking

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -5,17 +5,30 @@ import { useNavigation } from '@react-navigation/native';
 import getMatchedUserInfo from '../lib/getMatchedUserInfo';
 import { db } from '../firebase';
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { Match, UserProfile } from '../types/types';
 
-const ChatRow = ({matchDetails, }) => {
+interface ChatRowProps {
+  matchDetails: Match;
+}
+
+const ChatRow = ({ matchDetails }: ChatRowProps) => {
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // State Variables
+    const [matchedUserInfo, setMatchedUserInfo] = useState<UserProfile>(null);
+    const [lastMessage, setLastMessage] = useState<string>('');
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Navigation Prop
     const navigation = useNavigation();
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Custom Hook: useAuth()
     const { userUID } = useAuth();
-    const [matchedUserInfo, setMatchedUserInfo] = useState(null);
-    const [lastMessage, setLastMessage] = useState('');
-
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Extract the matched user's info using custom function
     useEffect(() => {
-        setMatchedUserInfo(getMatchedUserInfo(matchDetails.users, userUID));
+        setMatchedUserInfo(getMatchedUserInfo(matchDetails, userUID));
     }, [matchDetails, userUID]);
-
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Firestore Query: Get the last chat message sent if it exists
     useEffect(
         () =>
             onSnapshot(
@@ -26,25 +39,34 @@ const ChatRow = ({matchDetails, }) => {
             ),
         [matchDetails, db]
     );
-
-  return (
-    <TouchableOpacity
-        onPress={() => navigation.navigate('Message', {matchDetails,})}
-        style={styles.cardShadow}
-        className='flex-row items-center py-3 px-5 bg-white mx-3 my-1 rounded-lg'>
-      <Image
-        className='rounded-full h-16 w-16 mr-4'
-        source={{uri: matchedUserInfo?.photoURL}}
-      />
-      <View>
-        <Text className='text-lg font-semibold'>
-            {matchedUserInfo?.displayName}
-        </Text>
-        <Text>{lastMessage || "Be the first to message!"}</Text>
-      </View>
-    </TouchableOpacity>
-  )
-}
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    return (
+      // Base Container
+      <TouchableOpacity
+          onPress={() => navigation.navigate('Message', {matchDetails})}
+          style={styles.cardShadow}
+          className='flex-row items-center py-3 px-5 bg-primary-200 mx-3 my-1 rounded-lg'
+        >
+        {/* Matched User Image */}
+        <Image
+          className='rounded-full h-16 w-16 mr-4'
+          source={{uri: matchedUserInfo?.photoURL}}
+        />
+        {/* Chat Row Content */}
+        <View className='flex-1'>
+          {/* Matched User Name */}
+          <Text className='text-primary-950 text-lg font-semibold'>
+              {matchedUserInfo?.displayName}
+          </Text>
+          {/* Last Message Sent */}
+          <Text className='text-md'>
+            {lastMessage || "Be the first to message!"}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+};
 
 export default ChatRow
 
--- types/navigationTypes.ts
+++ types/navigationTypes.ts
@@ -2,12 +2,14 @@
 // https://reactnavigation.org/docs/typescript/#specifying-default-types-for-usenavigation-link-ref-etc
 
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { UserProfile } from './types';
+import { Match, UserProfile } from './types';
 
 export type RootStackParamList = {
     Home: undefined;
     Chat: undefined;
-    Message: undefined;
+    Message: {
+        matchDetails: Match;
+    };
     Match: {
         loggedInProfile: UserProfile;
         userSwiped: UserProfile;
